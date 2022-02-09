9950c98dcaeefbfa02b360ce030498553f2c73e0
Vlasis Mentzelopoulos <vlasism@gmail.com>
2022-02-09 14:13:01 +0200

    start creation of search

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,13 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -26,6 +27,7 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
   return (
     <GithubContext.Provider
       value={{ users: state.users, loading: state.loading, fetchUsers }}
